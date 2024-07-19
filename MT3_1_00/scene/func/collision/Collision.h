2e0d387899696b73124e991f7db67edb6df5cafe
sora <147121795+sora7716@users.noreply.github.com>
2024-07-19 16:21:18 +0900

    collisonクラスのコメントアウトをわかりやすく書き換えた

--- MT3_1_00/scene/func/collision/Collision.h
+++ MT3_1_00/scene/func/collision/Collision.h
@@ -12,37 +12,43 @@ class AABB;
 class Collision {
 
 public://メンバ関数
+
 	/// <summary>
 	///  当たり判定(球と球)
 	/// </summary>
-	/// <param name="target">当たり判定をしたい対象</param>
-	/// <param name="sphere">球</param>
+	/// <param name="target">ターゲット</param>
+	/// <param name="sphere1">球1</param>
+	/// <param name="sphere2">球2</param>
 	void IsCollision(Sphere* target, const GameObject::SphereMaterial& sphere1, const GameObject::SphereMaterial& sphere2);
 
 	/// <summary>
-	/// 当たり判定(球と平面)
+	///  当たり判定(球と平面)
 	/// </summary>
-	/// <param name="target">当たり判定をしたい対象</param>
+	/// <param name="target">ターゲット</param>
+	/// <param name="sphere">球</param>
 	/// <param name="plane">平面</param>
 	void IsCollision(Sphere* target, const GameObject::SphereMaterial& sphere, const GameObject::PlaneMaterial& plane);
 
 	/// <summary>
 	/// 当たり判定(線と平面)
 	/// </summary>
-	/// <param name="target"></param>
-	/// <param name="plane"></param>
+	/// <param name="target">ターゲット</param>
+	/// <param name="segment">線分</param>
+	/// <param name="plane">平面</param>
 	void IsCollision(Line* target, const GameObject::Segment& segment, const GameObject::PlaneMaterial& plane);
 
 	/// <summary>
 	/// 当たり判定(線と三角形)
 	/// </summary>
-	/// <param name="target"></param>
-	/// <param name="triangle"></param>
+	/// <param name="target">ターゲット</param>
+	/// <param name="segment">線分</param>
+	/// <param name="triangle">三角形</param>
 	void IsCollision(Line* target, const GameObject::Segment& segment, const GameObject::TriangleMaterial& triangle);
 
 	/// <summary>
 	/// AABBの当たり判定
 	/// </summary>
+	/// <param name="target">ターゲット</param>
 	/// <param name="aabb1">ボックス1</param>
 	/// <param name="aabb2">ボックス2</param>
 	void IsCollision(AABB* target, GameObject::AABBMaterial aabb1, GameObject::AABBMaterial aabb2);
